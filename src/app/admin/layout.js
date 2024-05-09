acd8b43c25394f6bde9f44d49b286401b2a77cdb
HarshitIndira <sharshit@indiratrade.com>
2024-05-09 15:45:15 +0530

    Front-end for admin, login and sinup page

---
+++ src/app/admin/layout.js
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en" className="h-full bg-white">
+            <body className="h-full">{children}</body>
+        </html >
+    );
+}
